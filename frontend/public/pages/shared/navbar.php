2f52c4c39ae119e9558af901ba4f8ecd5ebb9cbe
Lee Ren Jie <work.renjie@gmail.com>
2022-02-23 23:55:55 +0800

    add navbar

---
+++ frontend/public/pages/shared/navbar.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--BootStrap/css stylesheets-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<link rel="stylesheet" href="../../../src/stylesheets/nav.css" />
+		<!-- Pixel CSS -->
+		<link type="text/css" href="../../../src/stylesheets/neumorphism.css" rel="stylesheet">
+
+  </head>
+  <body>
+		<nav id="navbar-main" class="navbar navbar-expand-lg navbar-light">
+			<div class="container-fluid position-relative px-5">
+				<a href="">
+					<img src="../../images/logo.svg" alt="logo" class="logo" >
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="navbar-collapse collapse" id="navbarSupportedContent">
+					<div class="navbar-collapse-header">
+						<div class="row">
+							<div class="col-6 collapse-brand">
+								<a href="">
+									<img src="../../images/logo.svg" alt="logo" class="logo">
+								</a>
+							</div>
+							<div class="col-6 collapse-close">
+								<a href="#navbarSupportedContent" class="fas fa-times" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" title="close" aria-label="Toggle navigation"></a>
+							</div>
+						</div>
+					</div>
+					<ul class="navbar-nav navbar-nav-hover ml-auto">
+						<li class="nav-item dropdown mr-3">
+							<a href="#" class="nav-link" data-toggle="dropdown" >
+								<span class="nav-link-inner-text">
+									<i class="fas fa-user"></i>
+									User Name
+								</span>
+								<span class="fas fa-angle-down nav-link-arrow ml-1"></span>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a class="dropdown-item" href="">Profile</a></li>
+								<li><a class="dropdown-item" href="">Edit Password</a></li>
+								<li><a class="dropdown-item" href="">Log Out</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+    <!-- Jquery and Bootstrap CDN link for JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
