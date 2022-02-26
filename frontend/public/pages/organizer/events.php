7e831f66889185f45de5d3ce59d98d33ac0a6098
Lee Ren Jie <work.renjie@gmail.com>
2022-02-26 16:46:13 +0800

    shadow for content

--- frontend/public/pages/organizer/events.php
+++ frontend/public/pages/organizer/events.php
@@ -4,13 +4,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../../src/stylesheets/org-event.css" />
   <title>Organizer Home Page</title>
 </head>
 <body>
   <?php include '../shared/navbar.php';?>
   <div class="flex flex-row h-screen">
     <?php include '../shared/sidebar.php';?>
-    <div class="basis-10/12 overflow-auto">
+    <div class="basis-10/12 overflow-auto shadow">
     </div>
   </div>
 </body>
