6ebf2308d7815eaf5e4551436883f057d612b2b9
unknown <yeehaulee1@gmail.com>
2022-02-26 14:37:59 +0800

    renamed all file name to lowercase for admin html & css pages

--- frontend/public/pages/admin/New_admin.php
+++ frontend/public/pages/admin/New_admin.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/28d45fc291.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../../src/stylesheets/neumorphism.css">
-    <link rel="stylesheet" href="../../../src/stylesheets/admin-New_admin.css">
+    <link rel="stylesheet" href="../../../src/stylesheets/admin-new_admin.css">
     <title>Document</title>
 </head>
 <body>
--- frontend/public/pages/admin/Users.php
+++ frontend/public/pages/admin/Users.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/28d45fc291.js" crossorigin="anonymous"></script>
-    <link href="../../../src/stylesheets/admin-Users.css" rel="stylesheet">
+    <link href="../../../src/stylesheets/admin-users.css" rel="stylesheet">
     <link rel="stylesheet" href="../../../src/stylesheets/neumorphism.css">
     <title>Users Page</title>
 </head>
