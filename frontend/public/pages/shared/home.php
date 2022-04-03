0012255fe48337acb19098a5b693dd9568e11fbf
Lee Ren Jie <work.renjie@gmail.com>
2022-04-03 17:12:34 +0800

    home page

--- frontend/public/pages/shared/home.php
+++ frontend/public/pages/shared/home.php
@@ -11,7 +11,7 @@
 <body>
   <?php include '../shared/navbar.php';?>
   <div class="flex flex-row h-screen home-container">
-    <div class="overflow-y-auto overflow-x-hidden basis-full back-shadow">
+    <div class="overflow-y-auto overflow-x-hidden basis-full">
       <section id="title" class="title-container">
         <div class="title-text">
           <h1 class="bold">Judging System</h1>
@@ -79,44 +79,49 @@
         </div>
       </section>
       <section id="faq" class="faq-container mt-5">
-        <div class="faq-text text-center">
+        <div class="faq-text">
           <h2 class="bold mb-5">Frequently Asked Questions</h2>
           <div class="accordion shadow-soft rounded" id="accordionExample1">
             <div class="card card-sm card-body bg-primary border-light mb-0">
               <a href="#panel-1" data-target="#panel-1" class="accordion-panel-header" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="panel-1">
-                <span class="h6 mb-0 font-weight-bold">How to create events as an organizer</span>
+                <span class="h6 mb-0 font-weight-bold">How to create events as an organizer?</span>
                 <span class="icon"><span class="fas fa-plus"></span></span>
               </a>
-              <div class="collapse" id="panel-2">
+              <div class="collapse" id="panel-1">
                 <div class="pt-3">
                   <p class="mb-0">
-
+                    Click on my-events from the sidebar and click on create new event.
+                    Enter the relevant details in all the inputs provided and click on create.
                   </p>
                 </div>
               </div>
             </div>
             <div class="card card-sm card-body bg-primary border-light mb-0">
               <a href="#panel-2" data-target="#panel-2" class="accordion-panel-header" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="panel-1">
-                <span class="h6 mb-0 font-weight-bold">How to join events as a participant</span>
+                <span class="h6 mb-0 font-weight-bold">How to join events as a participant?</span>
                 <span class="icon"><span class="fas fa-plus"></span></span>
               </a>
-              <div class="collapse" id="panel-1">
+              <div class="collapse" id="panel-2">
                 <div class="pt-3">
                   <p class="mb-0">
-
+                    Click on view-events from the sidebar and click on any events.
+                    Click on participate at the event details page.
+                    Choose if you are a team leader or team member to register for the event.
                   </p>
                 </div>
               </div>
             </div>
             <div class="card card-sm card-body bg-primary border-light">
               <a href="#panel-3" data-target="#panel-3" class="accordion-panel-header" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="panel-1">
-                <span class="h6 mb-0 font-weight-bold">How to judge event as a judge</span>
+                <span class="h6 mb-0 font-weight-bold">How to judge event as a judge?</span>
                 <span class="icon"><span class="fas fa-plus"></span></span>
               </a>
               <div class="collapse" id="panel-3">
                 <div class="pt-3">
                   <p class="mb-0">
-
+                    Click on judge at the navbar.
+                    Enter the unique code provided by the organizer to join the event as a judge.
+                    Head over to the judgemnet page to start judging in the competiton.
                   </p>
                 </div>
               </div>
